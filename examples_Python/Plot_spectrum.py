d53554064ebac78aa46528da1a47e3f5b2444fa6
aivett <bilbao.aivett@gmail.com>
2023-06-20 15:40:27 -0700

    Include line to export metadata table as csv.

--- examples_Python/Plot_spectrum.py
+++ examples_Python/Plot_spectrum.py
@@ -1,19 +1,23 @@
 import h5py
 import hdf5plugin
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 mzafile = "test_data/LCMSMS_Lipids_POS.mza"
 
 mza = h5py.File(mzafile, 'r')
+print(list(mza.keys())) # h5py.File acts like a Python dictionary
+
 # Reading Metadata table:
 metadata = mza["Metadata"]
+# Save metadata table as csv:
+metadata = pd.DataFrame(np.array(metadata))
+metadata.to_csv(mzafile.replace(".mza",".csv"), index=False)
 
 # Reading spectrum with scan number 2431, 2 jagged arrays with the intensity and mz values:
 intensities = mza["Arrays_intensity/2431"][:]
-print(intensities)
 mz = mza["Arrays_mz/2431"][:]
-print(mz)
 
 mza.close()
 
